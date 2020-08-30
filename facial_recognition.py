ca77f8f56ba9bbc87abc88078a8f30f5195c7351
Jeremy Hudson Chan <jeremyhudsonchan@gmail.com>
2020-08-30 19:12:33 +0800

    Added comments to program

--- facial_recognition.py
+++ facial_recognition.py
@@ -6,37 +6,37 @@ import imutils
 import cv2
 import os
 
+# Init models
 model_face = cv2.CascadeClassifier('data/haarcascades/haarcascade_frontalface_default.xml')
 model_eye = cv2.CascadeClassifier('data/haarcascades/haarcascade_eye.xml')
-
-image_counter = 0
-
 classes = ["background", "aeroplane", "bicycle", "bird", "boat",
 "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
 "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
 "sofa", "train", "tvmonitor"]
-
 net = cv2.dnn.readNetFromCaffe("data/MobileNetSSD/MobileNetSSD_deploy.prototxt.txt", "data/MobileNetSSD/MobileNetSSD_deploy.caffemodel")
 colors = np.random.uniform(0, 255, size=(len(classes), 3))
 
+# Start camera
 vs = VideoStream().start()
 video_capture = cv2.VideoCapture(0)
-
 fps = FPS().start()
+
+# Log
+image_counter = 0
 
 while True:
     ret, frame = video_capture.read()
-
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = model_face.detectMultiScale(gray, 1.3, 5)
 
+    # Camera frame
     (h, w) = frame.shape[:2]
     blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 0.007843, (300, 300), 127.5)
     net.setInput(blob)
     detections = net.forward()
-    
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    # Detecting person with confidence
     for i in np.arange(0, detections.shape[2]):
         confidence = detections[0, 0, i, 2]
         if confidence > 0.75:
@@ -63,23 +63,31 @@ while True:
             cv2.rectangle(frame, (startX, startY), (endX, endY),
                 colors[idx], 2)
             cv2.putText(frame, label, (startX, y),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, colors[idx], 2)   
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, colors[idx], 2)
+            # Detecting face
             for (x,y,w,h) in faces:
                 cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
                 roi_gray = gray[y:y+h, x:x+w]
                 roi_color = frame[y:y+h, x:x+w]
                 eyes = model_eye.detectMultiScale(roi_gray)
+                
+                # Detecting eyes
                 for (ex,ey,ew,eh) in eyes:
                     cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
+    # Showing the frame
     cv2.imshow('frame',frame)
+
+    # Make Directory if does not exist
     if not os.path.exists('images'):
             os.makedirs('images')
             print("Created new directory " + 'images')
 
+    # Exit program with 'q', save image with 'spacebar'
     k = cv2.waitKey(1)
     if k % 256 & 0xFF == ord('q'):
         break
+    # Spacebar
     elif k % 256 == 32: 
         img_name = "opencv_frame_{}.png".format(image_counter)
         cv2.imwrite("images/frame%d.jpg" % image_counter, frame)
